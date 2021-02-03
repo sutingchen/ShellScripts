f6a9cc9e3ad1e0bc89e9a01dbb3cdef7c537e242
Chen Su Ting <wendy10935@hotmail.com>
2021-02-03 15:42:43 +1300

    Updated file-rename-scripts.sh

--- file-rename-script.sh
+++ file-rename-script.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 PREFIX='AWS_'
+TXT_TO_REMOVE='AWS_Certified_Security_Study_Guide_Specialty_(SCS-..._----_('
 EXT='pdf'
 
 
 find . -type f -name "${PREFIX}*.${EXT}" | while read FILE ; do
-    NEWFILE="$(echo ${FILE} |sed -e 's/AWS_Certified_Developer_Official_Study_Guide_Assoc..._----_(//')";
-    #echo ${NEWFILE};
+    NEWFILE=$(echo ${FILE} | sed -e "s/${TXT_TO_REMOVE}//");
+    echo ${NEWFILE};
     mv "${FILE}" "${NEWFILE}";
 done 
 
-for FILE in *.${EXT}
-do
-  #echo "${FILE}"
-  NEWFILE="${FILE%?.${EXT}}"
-  #echo "${NEWFILE}"
+find . -type f -name "*).${EXT}" | while read FILE ; do
+  echo "Old file name ${FILE}"
+  NEWFILE="${FILE%).${EXT}}"
+  echo " New file name is ${NEWFILE}"
   mv "${FILE}" "${NEWFILE}".${EXT}
 done
